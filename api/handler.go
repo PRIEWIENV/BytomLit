1be387859e2136885ad4283e30e986247d3e044e
HAOYUatHZ <haoyu@protonmail.com>
2019-08-12 23:41:17 +0800

    add logging req

--- api/handler.go
+++ api/handler.go
@@ -1,14 +1,28 @@
 package api
 
 import (
-	// "log"
+	"encoding/json"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
-type buildTxReq struct{}
+type buildTxReq struct {
+	Inputs  []io   `json:"inputs"`
+	Outputs []io   `json:"outputs"`
+	Memo    string `json:"memo"`
+}
+
+type io struct {
+	Program string `json:"program"`
+	AssetID string `json:"asset_id"`
+	Amount  uint64 `json:"amount"`
+}
 
 func (s *Server) BuildTx(c *gin.Context, req *buildTxReq) ([]string, error) {
-	// log.Println(req)
+	if b, err := json.Marshal(req); err == nil {
+		log.Println("received req:", string(b))
+	}
+
 	return nil, nil
 }
